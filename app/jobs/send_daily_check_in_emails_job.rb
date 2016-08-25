7b55a39ee2b7660a6ed3cd553b9acc91cad71444
Casey Brant <casey@caseybrant.com>
2016-08-24 20:39:49 -0500

    adjust logger messages for daily email job

--- app/jobs/send_daily_check_in_emails_job.rb
+++ app/jobs/send_daily_check_in_emails_job.rb
@@ -3,16 +3,20 @@ class SendDailyCheckInEmailsJob < ApplicationJob
 
   def perform(*args)
     today = Date.today
-    logger.debug "About to send out daily emails for #{today}!"
+    logger.info "============================================"
+    logger.info "About to send out daily emails for #{today}!"
+    logger.info "============================================"
 
     habits = Habit.where(active: true)
 
     habits.each do |habit|
       entry = habit.entries.create(for_date: today)
-      logger.debug "Sending email to #{habit.user_name} about #{habit.habit_name}..."
+      logger.info "Sending email to #{habit.user_name} about #{habit.habit_name}..."
       EntryMailer.daily_entry_email(entry).deliver_later
     end
 
-    logger.debug "Done with the daily emails for #{today}!"
+    logger.info "============================================"
+    logger.info "Done with the daily emails for #{today}!"
+    logger.info "============================================"
   end
 end
