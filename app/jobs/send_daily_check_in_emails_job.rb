6f70832590c392b37efc1c75d6c55d91796dccbd
Casey Brant <casey@caseybrant.com>
2016-09-07 19:14:58 -0500

    allow daily check in email job to accept any date as an argument

--- app/jobs/send_daily_check_in_emails_job.rb
+++ app/jobs/send_daily_check_in_emails_job.rb
@@ -1,22 +1,21 @@
 class SendDailyCheckInEmailsJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    today = Date.today
+  def perform(day=Date.today)
     logger.info "============================================"
-    logger.info "About to send out daily emails for #{today}!"
+    logger.info "About to send out daily emails for #{day}!"
     logger.info "============================================"
 
     habits = Habit.where(active: true)
 
     habits.each do |habit|
-      entry = habit.entries.create(for_date: today)
+      entry = habit.entries.create(for_date: day)
       logger.info "Sending email to #{habit.user_name} about #{habit.habit_name}..."
       EntryMailer.daily_entry_email(entry).deliver_later
     end
 
     logger.info "============================================"
-    logger.info "Done with the daily emails for #{today}!"
+    logger.info "Done with the daily emails for #{day}!"
     logger.info "============================================"
   end
 end
